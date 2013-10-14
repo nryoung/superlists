0160d8e49c5f5d6a7833b20f4db5e1c69da42693
Nic Young <nryoung@gmail.com>
2013-10-14 02:00:33 -0600

    Front page HTML now generated from a template. Fixed some typos in func. test.

--- functional_tests.py
+++ functional_tests.py
@@ -11,7 +11,7 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrive_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
 
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
@@ -23,7 +23,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_tag_name('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
                 inputbox.get_attribute('placeholder'),
                 'Enter a to-do item'
@@ -38,9 +38,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows)
+                any(row.text == '1: Buy peacock feathers' for row in rows),
+                "New to-do item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item. She
