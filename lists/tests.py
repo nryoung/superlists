077c8f6cb375e2acd6853852af586404ecbaf495
Nic Young <nryoung@gmail.com>
2013-10-14 01:04:47 -0600

    First unit test and url mapping, dummy view.

--- lists/tests.py
+++ lists/tests.py
@@ -6,11 +6,11 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from lists.views import home_page
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
