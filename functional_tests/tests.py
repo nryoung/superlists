e7ab0ac89cdfacb4f6bf230ab1f102795ce645a4
Nic Young <nryoung@gmail.com>
2013-10-21 23:41:32 -0600

    Moved functional tests to its own app, and now use LiveServerTestCase instead.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+from django.test import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -20,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists.
         self.assertIn('To-Do', self.browser.title)
@@ -62,7 +62,4 @@ class NewVisitorTest(unittest.TestCase):
 
         # She visits that URL - her to-do list is still there
 
-        # Satisfied, sh goes back to sleep
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+        # Satisfied, she goes back to sleep
