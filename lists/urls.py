26028eba3fce27da5b135eb6818eff96e541d186
Nic Young <nryoung@gmail.com>
2013-12-22 13:36:35 -0700

    Refactored list urls in to list.urls module.

--- lists/urls.py
+++ lists/urls.py
@@ -5,12 +5,9 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-     url(r'^$', 'lists.views.home_page', name='home'),
-     url(r'^lists/', include('lists.urls')),
-     #url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
-     #url(r'^lists/(\d+)/new_item$', 'lists.views.add_item', name='add_item'),
-     #url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+     url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
+     url(r'^(\d+)/new_item$', 'lists.views.add_item', name='add_item'),
+     url(r'^new$', 'lists.views.new_list', name='new_list'),
     # url(r'^superlists/', include('superlists.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
