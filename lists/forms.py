35119b03a2b047c8095dc3bc0fcf3fea30d1ba30
Nic Young <nryoung@gmail.com>
2014-01-08 01:29:54 -0700

    new form for list items

---
+++ lists/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from lists.models import Item
+
+EMPTY_LIST_ERROR = "You can't have an empty list item"
+
+
+class ItemForm(forms.models.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = {
+            'text': forms.fields.TextInput(attrs={
+                'placeholder': 'Enter a to-do item',
+                'class': 'form-control input-lg',
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['text'].error_messages['required'] = EMPTY_LIST_ERROR
---
+++ lists/tests/test_forms.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from lists.forms import ItemForm, EMPTY_LIST_ERROR
+
+
+class ItemFormTest(TestCase):
+
+    def test_form_item_input_has_placeholder_and_css_classes(self):
+        form = ItemForm()
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+        self.assertIn('class="form-control input-lg"', form.as_p())
+
+    def test_form_validation_for_blank_items(self):
+        form = ItemForm(data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors['text'],
+            [EMPTY_LIST_ERROR]
+        )
