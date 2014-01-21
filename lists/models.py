e573875dbbfd3760b9248d4485fd7bb0ec85e604
Nic Young <nryoung@gmail.com>
2014-01-20 23:30:31 -0700

    Implement duplicate item validation at model layer.

--- lists/models.py
+++ lists/models.py
@@ -6,10 +6,18 @@ class List(models.Model):
     def get_absolute_url(self):
         return resolve_url('view_list', self.id)
 
+
 class Item(models.Model):
     text = models.TextField()
     list = models.ForeignKey(List)
 
+    class Meta:
+        ordering = ('id',)
+        unique_together = ('list', 'text')
+
     def save(self, *args, **kwargs):
         self.full_clean()
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.text
--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -44,6 +44,28 @@ class ListAndItemModelsTest(TestCase):
         with self.assertRaises(ValidationError):
             item.save()
 
+    def test_CAN_save_same_item_to_different_lists(self):
+        list1 = List.objects.create()
+        list2 = List.objects.create()
+        Item.objects.create(list=list1, text='bla')
+        Item.objects.create(list=list2, text='bla')  # should not raise
+
     def test_get_absolute_url(self):
         list1 = List.objects.create()
         self.assertEqual(list1.get_absolute_url(), '/lists/%d/' % (list1.id,))
+
+    def test_list_ordering(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='item1')
+        item2 = Item.objects.create(list=list1, text='item2')
+        item3 = Item.objects.create(list=list1, text='item3')
+
+        self.assertEqual(
+            list(Item.objects.all()),
+            [item1, item2, item3]
+        )
+
+    def test_string_representation(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='some text')
+        self.assertEqual(str(item1), item1.text)
